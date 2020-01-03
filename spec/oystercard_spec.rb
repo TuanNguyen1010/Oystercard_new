eb08f62fa1a4b23e3a7ef4f86156618633d13d36
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-01-03 20:37:55 +0000

    top up method added & tested

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,11 +1,22 @@
 require 'oystercard'
 
 describe Oystercard do 
+  subject(:oystercard) {described_class.new}
 
     describe 'balance' do 
       it 'has a default balance of 0' do 
-        oystercard = Oystercard.new
         expect(oystercard.balance).to eq(0)
+      end 
+    end 
+
+    describe 'top up' do 
+      it 'respond top_up with 1 arguement' do 
+        expect(oystercard).to respond_to(:top_up).with(1).argument
+      end 
+
+      it 'it increase the balance by 10' do 
+        oystercard.top_up(10)
+        expect(oystercard.balance).to eq(10)
       end 
     end 
 end
