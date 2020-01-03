36af41a49a39a3d9201247e7343e0c6bdbe2e96e
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-01-03 20:24:29 +0000

    balance added

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,10 @@ require 'oystercard'
 
 describe Oystercard do 
 
+    describe 'balance' do 
+      it 'has a default balance of 0' do 
+        oystercard = Oystercard.new
+        expect(oystercard.balance).to eq(0)
+      end 
+    end 
 end
