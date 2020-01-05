611cacd5fad1cdeb2b77015f484aa495323adea3
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-01-05 02:12:27 +0000

    journey history added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,14 @@
 class Oystercard
-  attr_reader :balance, :entry_station
+  attr_reader :balance, :entry_station, :exit_station, :journeys
+
   MAXIMUM_BALANCE = 90
   MINIMUM_FARE = 1
 
   def initialize
     @balance = 0
     @entry_station
+    @exit_station
+    @journeys = []
   end 
 
   def top_up(amount)
@@ -22,8 +25,10 @@ class Oystercard
     @entry_station = entry_station
   end 
 
-  def touch_out
+  def touch_out(exit_station)
     deduct(MINIMUM_FARE)
+    @exit_station = exit_station
+    @journeys.push({entry_station: @entry_station, exit_station: @exit_station})
     @entry_station = nil
   end 
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,7 +3,9 @@ require 'oystercard'
 describe Oystercard do 
   subject(:oystercard) {described_class.new}
   let(:entry_station) {double :station}
-
+  let(:exit_station) {double :station}
+  let(:journey){ {entry_station: entry_station, exit_station: exit_station}}
+  
   describe 'balance' do 
     it 'has a default balance of 0' do 
       expect(oystercard.balance).to eq(0)
@@ -53,21 +55,42 @@ describe Oystercard do
     it 'changes in_journey to false after touch in and then touch out' do 
       oystercard.top_up(10)
       oystercard.touch_in(entry_station)
-      oystercard.touch_out
+      oystercard.touch_out(exit_station)
       expect(oystercard).not_to be_in_journey
     end 
 
     it 'deduct the minimum fare from balance when touch out' do 
       oystercard.top_up(10)
       oystercard.touch_in(entry_station)
-      expect{oystercard.touch_out}.to change{oystercard.balance}.by(-Oystercard::MINIMUM_FARE)
+      expect{oystercard.touch_out(exit_station)}.to change{oystercard.balance}.by(-Oystercard::MINIMUM_FARE)
     end
 
     it 'removes entry station when touch out' do 
       oystercard.top_up(10)
       oystercard.touch_in(entry_station)
-      oystercard.touch_out
-      expect(oystercard.entry_station).to eq nil
+      oystercard.touch_out(exit_station)
+      expect(oystercard.entry_station).to eq(nil)
     end 
+
+    it 'saves exit station when touch out' do 
+      oystercard.top_up(10)
+      oystercard.touch_in(entry_station)
+      oystercard.touch_out(exit_station)
+      expect(oystercard.exit_station).to eq(exit_station)
+    end 
+  end 
+
+  describe 'journeys' do 
+
+    it 'has an empty list of journeys by default' do
+      expect(oystercard.journeys).to be_empty
+    end
+
+    it 'stores a journey' do
+      oystercard.top_up(10)
+      oystercard.touch_in(entry_station)
+      oystercard.touch_out(exit_station)
+      expect(oystercard.journeys).to include journey
+    end
   end 
 end
